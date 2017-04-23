6f624a9265c37ec60f09f6f0bf29be526f66984e
sch1eiden <aiew_haha@hotmail.com>
2017-04-23 20:17:52 +0700

    Add files via upload
    Adding some elements into class

--- MovieReviewMain.java
+++ MovieReviewMain.java
@@ -4,6 +4,15 @@
  * and open the template in the editor.
  */
 package moviereviewmain;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.StringTokenizer;
 
 /**
  *
@@ -15,7 +24,17 @@ public class MovieReviewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        List<String> lines = readLine("movieReview.txt");
+        for (String a : lines) {
+            StringTokenizer token = new StringTokenizer(a, " ");
+            String message;
+            int score = Integer.parseInt(token.nextToken());
+           
+        }
+    }
+
+    private static List<String> readLine(String movieReviewtxt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
