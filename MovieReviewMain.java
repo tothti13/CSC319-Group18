8119ed70db18ef8df96082a2d2a35a0c9152c7be
sch1eiden <aiew_haha@hotmail.com>
2017-04-28 11:26:31 +0700

    done with errors

--- MovieReviewMain.java
+++ MovieReviewMain.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package moviereviewmain;
+package moviereview;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -14,27 +10,52 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.StringTokenizer;
 
-/**
- *
- * @author DELL PC
- */
 public class MovieReviewMain {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        List<String> lines = readLine("movieReview.txt");
-        for (String a : lines) {
-            StringTokenizer token = new StringTokenizer(a, " ");
-            String message;
-            int score = Integer.parseInt(token.nextToken());
-           
+  
+  public static void main(String args[]) throws IOException {
+    
+    HashTable table = new HashTable(2000);
+    
+    File file = new File("movieReviews.txt");
+    BufferedReader reader = null;
+    
+    int x = 1;
+    try{
+      reader = new BufferedReader(new FileReader(file));
+      if(reader.readLine() == null)
+      {
+        System.out.println("Cound not open file");
+      }
+      else{
+        table = new HashTable(2000);
+        while((line = reader.readLine()) != null)
+        {
+          score = Integer.parseInt(message.substring(0 , 1));
+          
+          message = line.substring(x , message.indexOf(" "));
+          x += message.indexOf(" ");
         }
-    }
-
-    private static List<String> readLine(String movieReviewtxt) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      }
+    } catch (FileNotFoundException e){
+    } catch(IOException e){
     }
     
+    
+    List<String> lines = readLines("movieReview.txt");
+    for (String a : lines) {
+      StringTokenizer token = new StringTokenizer(a, " ");
+      String message;
+      int score = Integer.parseInt(token.nextToken());
+      while (token.hasMoreTokens()) {
+        message = token.nextToken();
+        table.put(message, score);
+      }
+    }
+  }
+  
+  private static List<String> readLines(String file) throws IOException {
+    
+    return Files.readAllLines(Paths.get(file));
+    
+  }
 }
